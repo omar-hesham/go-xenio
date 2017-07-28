cb7d2e922b1d240fb7e9e8b71d9cd795fe0b5520
xenioplatform <admin@xenio.io>
2017-07-28 15:47:41 +0100

    Rebrand ENV
    Else it would fetch from the wrong repo

--- build/env.sh
+++ build/env.sh
@@ -10,7 +10,7 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
+ethdir="$workspace/src/github.com/xenioplatform"
 if [ ! -L "$ethdir/go-xenio" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
