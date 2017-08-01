d9c59358fbf2626e5236de75da93708f2820a623
root <admin@xenio.io>
2017-08-01 14:27:56 +0000

    modified:   tx_journal.go

--- core/tx_journal.go
+++ core/tx_journal.go
@@ -21,10 +21,10 @@ import (
 	"io"
 	"os"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/xenioplatform/go-xenio/common"
+	"github.com/xenioplatform/go-xenio/core/types"
+	"github.com/xenioplatform/go-xenio/log"
+	"github.com/xenioplatform/go-xenio/rlp"
 )
 
 // errNoActiveJournal is returned if a transaction is attempted to be inserted
