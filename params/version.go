4662e63e9d50fa671661fce5fcc9cb31b2e2d5ca
Andriopoulos Nikolaos <sirnicolas21@gmail.com>
2017-08-03 21:13:25 +0300

    xenio version change.

--- params/version.go
+++ params/version.go
@@ -21,17 +21,18 @@ import (
 )
 
 const (
-	VersionMajor = 1          // Major version component of the current release
-	VersionMinor = 7          // Minor version component of the current release
-	VersionPatch = 0          // Patch version component of the current release
+	VersionMajor = 0          // Major version component of the current release
+	VersionMinor = 0          // Minor version component of the current release
+	VersionPatch = 1          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
+	VersionFork = "Xenio"     // Fork metadata to append to the version string
 )
 
 // Version holds the textual version string.
 var Version = func() string {
 	v := fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
 	if VersionMeta != "" {
-		v += "-" + VersionMeta
+		v += "-" + VersionMeta + "-" + VersionFork
 	}
 	return v
 }()
