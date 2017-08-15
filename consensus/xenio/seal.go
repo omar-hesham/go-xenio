cc75e924a37d24f0037f8b7668c551b72f5416f9
Andriopoulos Nikolaos <sirnicolas21@gmail.com>
2017-08-15 15:24:19 +0300

    clean up

--- consensus/xenio/seal.go
+++ consensus/xenio/seal.go
@@ -13,41 +13,6 @@ import (
 	"github.com/xenioplatform/go-xenio/rlp"
 )
 
-// Seal implements consensus.Engine, attempting to find a nonce that satisfies
-// the block's difficulty requirements.
-/*func (ethash *Xenio) Seal(chain consensus.ChainReader, block *types.Block, stop <-chan struct{}) (*types.Block, error) {
-	// Create a runner and the multiple search threads it directs
-	abort := make(chan struct{})
-	found := make(chan *types.Block)
-
-	var pend sync.WaitGroup
-	for i := 0; i < threads; i++ {
-		pend.Add(1)
-		go func(id int, nonce uint64) {
-			defer pend.Done()
-			ethash.mine(block, id, nonce, abort, found)
-		}(i, uint64(ethash.rand.Int63()))
-	}
-	// Wait until sealing is terminated or a nonce is found
-	var result *types.Block
-	select {
-	case <-stop:
-		// Outside abort, stop all miner threads
-		close(abort)
-	case result = <-found:
-		// One of the threads found a block, abort all others
-		close(abort)
-	case <-ethash.update:
-		// Thread count was changed on user request, restart
-		close(abort)
-		pend.Wait()
-		return ethash.Seal(chain, block, stop)
-	}
-	// Wait for all miners to terminate and return the block
-	pend.Wait()
-	return result, nil
-}*/
-
 func (c *Xenio) Seal(chain consensus.ChainReader, block *types.Block, stop <-chan struct{}) (*types.Block, error) {
 	header := block.Header()
 
@@ -105,56 +70,6 @@ func (c *Xenio) Seal(chain consensus.ChainReader, block *types.Block, stop <-cha
 
 	return block.WithSeal(header), nil
 }
-
-// stake is the actual proof-of-work miner that searches for a nonce starting from
-// seed that results in correct final block difficulty.
-/*
-func (ethash *Xenio) stake(block *types.Block, id int, seed uint64, abort chan struct{}, found chan *types.Block) {
-	// Extract some data from the header
-	var (
-		header = block.Header()
-		hash   = header.HashNoNonce().Bytes()
-		target = new(big.Int).Div(maxUint256, header.Difficulty)
-
-		number  = header.Number.Uint64()
-		dataset = ethash.dataset(number)
-	)
-	// Start generating random nonces until we abort or find a good one
-	var (
-		nonce    = seed
-	)
-	logger := log.New("staker", id)
-	logger.Trace("Started staking with 13 eyes", "stake", seed)
-	for {
-		select {
-		case <-abort:
-			// Staking terminated, update stats and abort
-			logger.Trace("staking operation aborted", "attempts", nonce-seed)
-			return
-
-		default:
-			// Compute the PoW value of this nonce
-			digest, result := hashimotoFull(dataset, hash, nonce)
-			if new(big.Int).SetBytes(result).Cmp(target) <= 0 {
-				// Correct nonce found, create a new header with it
-				header = types.CopyHeader(header)
-				header.Nonce = types.EncodeNonce(nonce)
-				header.MixDigest = common.BytesToHash(digest)
-
-				// Seal and return a block (if still needed)
-				select {
-				case found <- block.WithSeal(header):
-					logger.Trace("Ethash nonce found and reported", "attempts", nonce-seed, "nonce", nonce)
-				case <-abort:
-					logger.Trace("Ethash nonce found but discarded", "attempts", nonce-seed, "nonce", nonce)
-				}
-				return
-			}
-			nonce++
-		}
-	}
-}
-*/
 
 func sigHash(header *types.Header) (hash common.Hash) {
 	hasher := sha3.NewKeccak256()
