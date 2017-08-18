fd4d3bca7d8e61d96d4a223e066126e422ec10ee
Andriopoulos Nikolaos <sirnicolas21@gmail.com>
2017-08-18 08:24:50 -0700

    major refactor

--- consensus/xenio/api.go
+++ consensus/xenio/api.go
@@ -1,37 +1,94 @@
+// Copyright 2017 The go-xenio Authors
+// This file is part of the go-xenio library.
+//
+// The go-xenio library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-xenio library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-xenio library. If not, see <http://www.gnu.org/licenses/>.
+
 package xenio
 
 import (
 	"github.com/xenioplatform/go-xenio/common"
 	"github.com/xenioplatform/go-xenio/consensus"
-	"github.com/xenioplatform/go-xenio/log"
-
-	//"github.com/xenioplatform/go-xenio/core/types"
-	//"github.com/xenioplatform/go-xenio/rpc"
+	"github.com/xenioplatform/go-xenio/core/types"
+	"github.com/xenioplatform/go-xenio/rpc"
 )
 
+// API is a user facing RPC API to allow controlling the signer and voting
+// mechanisms of the proof-of-authority scheme.
 type API struct {
 	chain  consensus.ChainReader
 	xenio *Xenio
 }
 
-// Discard drops a currently running staker.
-func (api *API) Discard(address common.Address) {
-	api.xenio.lock.Lock()
-	defer api.xenio.lock.Unlock()
-
-	delete(api.xenio.stakers, address)
+// GetSnapshot retrieves the state snapshot at a given block.
+func (api *API) GetSnapshot(number *rpc.BlockNumber) (*Snapshot, error) {
+	// Retrieve the requested block number (or current if none requested)
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = api.chain.CurrentHeader()
+	} else {
+		header = api.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+	// Ensure we have an actually valid block and return its snapshot
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	return api.xenio.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
 }
 
-// Propose injects a new authorization proposal that the signer will attempt to
-// push through.
-func (api *API) Propose(address common.Address, auth bool) {
-	log.Warn("Propose: propose")
-	api.xenio.lock.Lock()
-	defer api.xenio.lock.Unlock()
-
-	api.xenio.proposals[address] = auth
+// GetSnapshotAtHash retrieves the state snapshot at a given block.
+func (api *API) GetSnapshotAtHash(hash common.Hash) (*Snapshot, error) {
+	header := api.chain.GetHeaderByHash(hash)
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	return api.xenio.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
 }
 
+// GetSigners retrieves the list of authorized signers at the specified block.
+func (api *API) GetSigners(number *rpc.BlockNumber) ([]common.Address, error) {
+	// Retrieve the requested block number (or current if none requested)
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = api.chain.CurrentHeader()
+	} else {
+		header = api.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+	// Ensure we have an actually valid block and return the signers from its snapshot
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	snap, err := api.xenio.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return snap.signers(), nil
+}
+
+// GetSignersAtHash retrieves the state snapshot at a given block.
+func (api *API) GetSignersAtHash(hash common.Hash) ([]common.Address, error) {
+	header := api.chain.GetHeaderByHash(hash)
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	snap, err := api.xenio.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return snap.signers(), nil
+}
+
+// Proposals returns the current proposals the node tries to uphold and vote on.
 func (api *API) Proposals() map[common.Address]bool {
 	api.xenio.lock.RLock()
 	defer api.xenio.lock.RUnlock()
@@ -42,3 +99,22 @@ func (api *API) Proposals() map[common.Address]bool {
 	}
 	return proposals
 }
+
+// Propose injects a new authorization proposal that the signer will attempt to
+// push through.
+func (api *API) Propose(address common.Address, auth bool) {
+	api.xenio.lock.Lock()
+	defer api.xenio.lock.Unlock()
+
+	api.xenio.proposals[address] = auth
+}
+
+// Discard drops a currently running proposal, stopping the signer from casting
+// further votes (either for or against).
+func (api *API) Discard(address common.Address) {
+	api.xenio.lock.Lock()
+	defer api.xenio.lock.Unlock()
+
+	delete(api.xenio.proposals, address)
+}
+
--- consensus/xenio/snapshot.go
+++ consensus/xenio/snapshot.go
@@ -1,16 +1,30 @@
+// Copyright 2017 The go-xenio Authors
+// This file is part of the go-xenio library.
+//
+// The go-xenio library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-xenio library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-xenio library. If not, see <http://www.gnu.org/licenses/>.
+
 package xenio
 
 import (
-"bytes"
-"encoding/json"
+	"bytes"
+	"encoding/json"
 
-"github.com/xenioplatform/go-xenio/common"
-"github.com/xenioplatform/go-xenio/consensus"
-"github.com/xenioplatform/go-xenio/core/types"
-"github.com/xenioplatform/go-xenio/ethdb"
-"github.com/xenioplatform/go-xenio/params"
-"github.com/xenioplatform/go-xenio/log"
-lru "github.com/hashicorp/golang-lru"
+	"github.com/xenioplatform/go-xenio/common"
+	"github.com/xenioplatform/go-xenio/core/types"
+	"github.com/xenioplatform/go-xenio/ethdb"
+	"github.com/xenioplatform/go-xenio/params"
+	lru "github.com/hashicorp/golang-lru"
 )
 
 // Vote represents a single vote that an authorized signer made to modify the
@@ -46,7 +60,6 @@ type Snapshot struct {
 // method does not initialize the set of recent signers, so only ever use if for
 // the genesis block.
 func newSnapshot(config *params.XenioConfig, sigcache *lru.ARCCache, number uint64, hash common.Hash, signers []common.Address) *Snapshot {
-	log.Warn("Snapshot: new")
 	snap := &Snapshot{
 		config:   config,
 		sigcache: sigcache,
@@ -64,8 +77,7 @@ func newSnapshot(config *params.XenioConfig, sigcache *lru.ARCCache, number uint
 
 // loadSnapshot loads an existing snapshot from the database.
 func loadSnapshot(config *params.XenioConfig, sigcache *lru.ARCCache, db ethdb.Database, hash common.Hash) (*Snapshot, error) {
-	log.Warn("Snapshot: load")
-	blob, err := db.Get(append([]byte("clique-"), hash[:]...))
+	blob, err := db.Get(append([]byte("xenio-"), hash[:]...))
 	if err != nil {
 		return nil, err
 	}
@@ -81,7 +93,6 @@ func loadSnapshot(config *params.XenioConfig, sigcache *lru.ARCCache, db ethdb.D
 
 // store inserts the snapshot into the database.
 func (s *Snapshot) store(db ethdb.Database) error {
-	log.Warn("Snapshot: store")
 	blob, err := json.Marshal(s)
 	if err != nil {
 		return err
@@ -91,7 +102,6 @@ func (s *Snapshot) store(db ethdb.Database) error {
 
 // copy creates a deep copy of the snapshot, though not the individual votes.
 func (s *Snapshot) copy() *Snapshot {
-	log.Warn("Snapshot: copy")
 	cpy := &Snapshot{
 		config:   s.config,
 		sigcache: s.sigcache,
@@ -163,7 +173,6 @@ func (s *Snapshot) uncast(address common.Address, authorize bool) bool {
 // apply creates a new authorization snapshot by applying the given headers to
 // the original one.
 func (s *Snapshot) apply(headers []*types.Header) (*Snapshot, error) {
-	log.Warn("Snapshot: apply")
 	// Allow passing in no headers for cleaner code
 	if len(headers) == 0 {
 		return s, nil
@@ -298,82 +307,4 @@ func (s *Snapshot) inturn(number uint64, signer common.Address) bool {
 		offset++
 	}
 	return (number % uint64(len(signers))) == uint64(offset)
-}
-
-// snapshot retrieves the authorization snapshot at a given point in time.
-func (c *Xenio) snapshot(chain consensus.ChainReader, number uint64, hash common.Hash, parents []*types.Header) (*Snapshot, error) {
-	// Search for a snapshot in memory or on disk for checkpoints
-	log.Warn("Snapshot: snapshot")
-	var (
-		headers []*types.Header
-		snap    *Snapshot
-	)
-	for snap == nil {
-		// If an in-memory snapshot was found, use that
-		if s, ok := c.recents.Get(hash); ok {
-			snap = s.(*Snapshot)
-			break
-		}
-		// If an on-disk checkpoint snapshot can be found, use that
-		if number%checkpointInterval == 0 {
-			if s, err := loadSnapshot(c.config, c.signatures, c.db, hash); err == nil {
-				log.Trace("Loaded voting snapshot form disk", "number", number, "hash", hash)
-				snap = s
-				break
-			}
-		}
-		// If we're at block zero, make a snapshot
-		if number == 0 {
-			genesis := chain.GetHeaderByNumber(0)
-			if err := c.VerifyHeader(chain, genesis, false); err != nil {
-				return nil, err
-			}
-			signers := make([]common.Address, (len(genesis.Extra)-extraVanity-extraSeal)/common.AddressLength)
-			for i := 0; i < len(signers); i++ {
-				copy(signers[i][:], genesis.Extra[extraVanity+i*common.AddressLength:])
-			}
-			snap = newSnapshot(c.config, c.signatures, 0, genesis.Hash(), signers)
-			if err := snap.store(c.db); err != nil {
-				return nil, err
-			}
-			log.Trace("Stored genesis voting snapshot to disk")
-			break
-		}
-		// No snapshot for this header, gather the header and move backward
-		var header *types.Header
-		if len(parents) > 0 {
-			// If we have explicit parents, pick from there (enforced)
-			header = parents[len(parents)-1]
-			if header.Hash() != hash || header.Number.Uint64() != number {
-				return nil, consensus.ErrUnknownAncestor
-			}
-			parents = parents[:len(parents)-1]
-		} else {
-			// No explicit parents (or no more left), reach out to the database
-			header = chain.GetHeader(hash, number)
-			if header == nil {
-				return nil, consensus.ErrUnknownAncestor
-			}
-		}
-		headers = append(headers, header)
-		number, hash = number-1, header.ParentHash
-	}
-	// Previous snapshot found, apply any pending headers on top of it
-	for i := 0; i < len(headers)/2; i++ {
-		headers[i], headers[len(headers)-1-i] = headers[len(headers)-1-i], headers[i]
-	}
-	snap, err := snap.apply(headers)
-	if err != nil {
-		return nil, err
-	}
-	c.recents.Add(snap.Hash, snap)
-
-	// If we've generated a new checkpoint snapshot, save to disk
-	if snap.Number%checkpointInterval == 0 && len(headers) > 0 {
-		if err = snap.store(c.db); err != nil {
-			return nil, err
-		}
-		log.Trace("Stored voting snapshot to disk", "number", snap.Number, "hash", snap.Hash)
-	}
-	return snap, err
 }
